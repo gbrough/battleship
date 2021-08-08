f770397fffb7e852e3c4f2b1a6788f9d8b0a7875
gbrough <gbrough@gmail.com>
2021-08-07 18:58:51 -0700

    add game requirements

---
+++ battleship.py
@@ -0,0 +1,11 @@
+'''
+1. Player guess ship in fewest of tries, limit of 50 tries
+2. Player game - 2 players take turns shooting at each other
+3. computer random plays against me with a random guess
+4. computer intelligence 
+ - add classes and objects
+ - base class ship
+ - derived class for each type of ship
+ - dictionary of ships and their location
+ 
+ '''
