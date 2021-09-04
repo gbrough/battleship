9736fbef511c49a2cf8af3c44be32cfaa3c3d25c
gbrough <gbrough@gmail.com>
2021-09-04 11:13:15 -0500

    Update battleship_OO.py

--- battleship_OO.py
+++ battleship_OO.py
@@ -3,52 +3,58 @@ class Battleship:
       self.name = name
       self.size = size
       self.location = location
-      self.hit = False
-      self.sunk = False
-      self.ships = []
-      self.ships.append(Battleship("Aircraft Carrier", 5, [1, 1]))
-      self.ships.append(Battleship("Battleship", 4, [1, 2]))
-      self.ships.append(Battleship("Submarine", 3, [1, 3]))
-      self.ships.append(Battleship("Destroyer", 3, [1, 4]))
-      self.ships.append(Battleship("Patrol Boat", 2, [1, 5]))
-  def hit_ship(self):
-      self.hit = True
-      return self.hit
-  def sunk_ship(self):
-      self.sunk = True
-      return self.sunk
-  def get_ship_type(self):
-      return self.ship_type
-  def get_ship_size(self):
-      return self.ship_size
-  def get_ship_location(self):
-      return self.location
   def get_ship_name(self):
       return self.name
+  def get_ship_size(self):
+      return self.size
+  def get_ship_location(self):
+      return self.location
 
 class board:
-  def __init__(self, size):
-      self.size = size
+  def __init__(self, battleship, width, height):
+      self.battleship = battleship
+      self.width = width
+      self.height = height
       self.board = []
-      for i in range(size):
-          self.board.append(["O"] * size)
+      for i in range(height):
+          self.board.append([])
+          for j in range(width):
+              self.board[i].append('~')
   def print_board(self):
-      for row in self.board:
-          print(" ".join(row))
-  def get_board(self):
-      return self.board
-
+      for i in range(self.height):
+          print(self.board[i])
+        
 class Player:
   def __init__(self, name, board):
       self.name = name
       self.board = board
 
+def run():
+  battleships = [
+      Battleship("Aircraft Carrier", 5, [0,0]),
+      Battleship("Battleship", 4, [0,1]),
+      Battleship("Submarine", 3, [0,2]),
+      Battleship("Destroyer", 3, [0,3]),
+      Battleship("Patrol Boat", 2, [0,4])
+  ]
+  #create players
+  user = Player("user", board(battleships[0], 5, 5))
+  computer = Player("computer", board(battleships[0], 5, 5))
+  #place ships
+  user.board.battleship.location = [0,0]
+  computer.board.battleship.location = [0,0]
+  #print board
+  user.board.print_board()
+  computer.board.print_board()
+
 if __name__ == '__main__':
-  #create instance of players
-  player1 = Player("Player", "board")
-  computer = Player("Computer", "board")
-  print(player1.name)
-  print(computer.name)
+  run()
+
+
+
+
+
+
 
   
 
